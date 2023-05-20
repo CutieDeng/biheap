af8ab03782e6b28fb524829aa4ffb5eaafe01c06
Cutie Deng <CutieDeng@iCloud.com>
2023-05-20 21:48:56 +0800

    Add `bubble_down` op.

--- src/core/bubble.rs
+++ src/core/bubble.rs
@@ -3,7 +3,7 @@ use std::mem::swap;
 use super::*; 
 
 impl <T: Ord> BiHeap<T> {
-    pub fn bubble_up<const IS_MIN_FIRST: bool>(&mut self, index: usize) {
+    pub(crate) fn bubble_up<const IS_MIN_FIRST: bool>(&mut self, index: usize) {
         let mut borrow = self.0.borrow_mut(); 
         debug_assert!(index < borrow.len());
         let mut slice = borrow.views_mut(); 
@@ -48,6 +48,84 @@ impl <T: Ord> BiHeap<T> {
             } else {
                 break; 
             }
+        }
+    }
+}
+
+impl <T: Ord> BiHeap<T> {
+    pub(crate) fn bubble_down<const IS_MIN_FIRST: bool>(&mut self, index: usize) {
+        let mut borrow = self.0.borrow_mut(); 
+        debug_assert!(index < borrow.len()); 
+        let mut slice = borrow.views_mut(); 
+        let slice = 
+            if IS_MIN_FIRST {
+                &mut slice[0] 
+            } else {
+                &mut slice[1] 
+            }; 
+        let mut index = index; 
+        loop {
+            let left_index = index * 2 + 1; 
+            if left_index >= slice.len() {
+                break; 
+            } 
+            let (mut split1, mut split2) = slice.split_at_mut(left_index); 
+            let this = &mut split1[index]; 
+            let (mut left, mut others) = split2.split_first_mut().unwrap(); 
+            let right = others.first_mut(); 
+            let select; 
+            let cell_ref; 
+            let should_swap; 
+            if let Some(right) = right {
+                let left_ref = left.borrow(); 
+                let right_ref = right.borrow(); 
+                if IS_MIN_FIRST {
+                    if left_ref.value < right_ref.value {
+                        select = left_index; 
+                        drop(left_ref); 
+                        cell_ref = left; 
+                    } else {
+                        select = left_index + 1;  
+                        drop(right_ref); 
+                        cell_ref = right; 
+                    }
+                } else {
+                    if left_ref.value > right_ref.value {
+                        select = left_index; 
+                        drop(left_ref); 
+                        cell_ref = left; 
+                    } else {
+                        select = left_index + 1; 
+                        drop(right_ref); 
+                        cell_ref = right; 
+                    } 
+                }
+            } else {
+                select = left_index; 
+                cell_ref = left; 
+            }
+            if IS_MIN_FIRST {
+                let this = this.borrow(); 
+                let cell = cell_ref.borrow(); 
+                should_swap = this.value > cell.value; 
+            } else {
+                let this = this.borrow(); 
+                should_swap = this.value < cell_ref.borrow().value; 
+            }
+            if !should_swap {
+                break; 
+            }
+            swap(this, cell_ref); 
+            let mut this = this.borrow_mut(); 
+            let mut cell = cell_ref.borrow_mut(); 
+            if IS_MIN_FIRST {
+                this.min_index = select; 
+                cell.min_index = index; 
+            } else {
+                this.max_index = select; 
+                cell.max_index = index; 
+            } 
+            index = select; 
         }
     }
 }

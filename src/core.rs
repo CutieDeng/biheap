617261bf3918186361f29ff6e43a3906edae7b4f
Cutie Deng <CutieDeng@iCloud.com>
2023-05-20 14:41:50 +0800

    Add `Handle` abstraction.

--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,19 @@
-use std::{rc::Rc, cell::RefCell};
+use std::{rc::{Rc, Weak}, cell::RefCell};
 
 use crate::bivec::BiVec;
 
-pub struct BiHeap <T: Ord> (pub(crate) Rc<RefCell<BiVec<T>>>); 
+pub struct BiHeap <T: Ord> (pub(crate) Rc<RefCell<BiVec<Rc<RefCell<Node<T>>>>>>); 
+
+pub struct Node <T> {
+    pub(crate) value: T, 
+    pub(crate) min_index: usize, 
+    pub(crate) max_index: usize, 
+}
+
+pub struct Handle <T> {
+    pub(crate) node_ref: Weak<RefCell<Node<T>>>, 
+    pub(crate) heap_ref: Weak<RefCell<BiVec<Rc<RefCell<Node<T>>>>>>, 
+}
 
 mod construct;
+mod push;
---
+++ src/core/push.rs
@@ -0,0 +1,24 @@
+use super::*; 
+
+impl <T: Ord> BiHeap<T> {
+    pub fn push(&mut self, value: T) -> Handle<T> {
+        let mut borrow = self.0.borrow_mut(); 
+        let len = borrow.len(); 
+        let value_rc = Rc::new(RefCell::new(
+            Node {
+                value,
+                min_index: len, 
+                max_index: len,
+            }
+        )); 
+        let value_rc2 = value_rc.clone(); 
+        let node_ref = Rc::downgrade(&value_rc); 
+        borrow.push(value_rc, value_rc2); 
+        drop(borrow); 
+        let handle = Handle {
+            node_ref, 
+            heap_ref: Rc::downgrade(&self.0), 
+        }; 
+        handle
+    }
+}
